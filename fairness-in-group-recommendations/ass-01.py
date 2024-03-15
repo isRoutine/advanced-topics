cbdbeb34f6533f625bfea7ef93799efe1d16d35c
gianluca farinaccio <gianluca.farinaccio@gmail.com>
2024-03-15 12:28:28 +0100

    top 10 similarity

--- fairness-in-group-recommendations/ass-01.py
+++ fairness-in-group-recommendations/ass-01.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import math
 
 USER_A = 4
 
 # import csv file and drop unuseful colunmns
 ratings = pd.read_csv("dataset/ratings.csv").drop(['timestamp'], axis=1)
-
 userA_ratings = ratings[ratings['userId'] == USER_A] # dataframe that contains ratings of USER A
 ratings = ratings[ratings['userId'] != USER_A] # dataframe that contains ratings of all other users
+
+similarity = dict() # dict for similarity { key='userid' : value = similarity}
 
 # rows are ordered by userId, analyze the dataframe for all users
 while ratings.shape[0] > 0:
@@ -19,8 +21,16 @@ while ratings.shape[0] > 0:
     commonFilms = pd.merge(userA_ratings, userB_ratings, how ='inner', on =['movieId'])
 
     if commonFilms.empty != True: # correlation on ratings for common movies
-        similarity = commonFilms['rating_x'].corr(commonFilms['rating_y'])
-        print('Similarity user '+ str(USER_A) + '& user', str(userB_id), ' --> ', similarity)
 
-    # remove from the dataframe the rows(ratings of otherUser) analyzed in this iteration
+        sim = commonFilms['rating_x'].corr(commonFilms['rating_y'])
+        if not math.isnan(sim): #drop the user when similarity is nan
+            similarity.update({userB_id : sim})
+        #print('Similarity user '+ str(USER_A) + '& user', str(userB_id), ' --> ', similarity)
+
+    # remove from the dataframe the rows(ratings of user B) analyzed in this iteration
     ratings = ratings[ratings['userId'] != userB_id]
+
+# sort similarity and take top 10
+similarity = dict(sorted(similarity.items(), key=lambda x:x[1], reverse=True)[:10])
+
+print(similarity)
